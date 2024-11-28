9bdb3c036d1e507a27004c0477c3b6a66258fe9e
Sergio <sar813@alumnos.unican.es>
2024-11-28 16:08:57 +0100

    Spring Initializr

---
+++ src/main/java/gestionPeluqueria/GestionDeUnaPeluqueriaApplication.java
@@ -0,0 +1,13 @@
+package gestionPeluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeUnaPeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeUnaPeluqueriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestionPeluqueria/GestionDeUnaPeluqueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionPeluqueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUnaPeluqueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
