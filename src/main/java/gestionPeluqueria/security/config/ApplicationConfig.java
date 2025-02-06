13d9a26118fe195281da446284a102817b53eb1d
Sergio <sar813@alumnos.unican.es>
2025-02-07 00:21:02 +0100

    URL frontend change

--- src/main/java/gestionPeluqueria/security/config/ApplicationConfig.java
+++ src/main/java/gestionPeluqueria/security/config/ApplicationConfig.java
@@ -55,7 +55,7 @@ public class ApplicationConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("https://white-tree-0e633e903.4.azurestaticapps.net");
+        config.addAllowedOrigin("https://witty-moss-06101b803.4.azurestaticapps.net");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
