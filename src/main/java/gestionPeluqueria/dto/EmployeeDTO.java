0272c8312b4095188fabec82170285a9b79160e6
Sergio <sar813@alumnos.unican.es>
2025-02-13 22:21:44 +0100

    Minor changes in DTOs

--- src/main/java/gestionPeluqueria/dto/EmployeeDTO.java
+++ src/main/java/gestionPeluqueria/dto/EmployeeDTO.java
@@ -5,18 +5,28 @@ import java.time.LocalDate;
 
 public class EmployeeDTO {
 
+    private long id;
     private String fullName;
     private String email;
     private String telephone;
     private LocalDate birthDate;
     private Role role;
 
-    public EmployeeDTO(String name, String email, String telephone, LocalDate birthDate, Role role) {
+    public EmployeeDTO(long id, String name, String email, String telephone, LocalDate birthDate, Role role) {
+        this.id = id;
         this.fullName = name;
         this.email = email;
         this.telephone = telephone;
         this.birthDate =  birthDate;
         this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getFullName() {
